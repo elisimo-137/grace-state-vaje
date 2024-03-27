8d4aa7200a99c26e337b2724ad49837a0bb2f973
elisimo-137 <ela.r.macek@gmail.com>
2024-03-27 18:03:33 +0100

    state vaje v2

--- src/components/LightSwitch.jsx
+++ src/components/LightSwitch.jsx
@@ -7,5 +7,14 @@ export function LightSwitch() {
   // Uporabi useEffect za dodajanje ali odstranjevanje classa `dark` na `document.documentElement` glede na vrednost `isDark`
 
   // Prikazi Switch komponento in ji posreduj `isDark` in `setIsDark` kot props
-  return <div>To je LightSwitch.jsx</div>;
+
+  const [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add(isDark ? "dark" : "light");
+  }, [isDark]);
+
+  return <Switch checked={isDark} onCheckedChange={setIsDark}></Switch>;
 }
--- src/components/TextLengthCounter.jsx
+++ src/components/TextLengthCounter.jsx
@@ -1,5 +1,30 @@
 import { useEffect, useState } from "react";
+import { Textarea } from "./ui/textarea";
+import { Slider } from "./ui/slider";
 
 export function TextLengthCounter() {
-  return <div>To je TextLengthCounter.jsx</div>;
+  const [text, setText] = useState("");
+  const [maxChars, setMaxChars] = useState(100);
+  const [isOverLimit, setIsOverLimit] = useState(false);
+
+  useEffect(() => {
+    if (text.length > maxChars) {
+      console.log("TOO MUCH TEXT!");
+      setIsOverLimit(true);
+    }
+  }, [text, maxChars]);
+
+  return (
+    <div>
+      <Textarea
+        value={text}
+        onChange={(t) => setText(t.target.value)}
+      ></Textarea>
+      <Slider
+        value={[maxChars]}
+        onValueChange={(new_value) => setMaxChars(new_value[0])}
+      ></Slider>
+      {isOverLimit ? "prevec znakov" : "dovolj znakov"}
+    </div>
+  );
 }
